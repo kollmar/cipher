0fe9a7968ddf9706718c2d1f625eb20c776257a0
Andre Kollmar <andre.kollmar@raven51.de>
2022-07-18 14:34:31 +0200

    Add decryption

--- test/index.js
+++ test/index.js
@@ -1,15 +1,26 @@
-const encrypt = require('../index.js').encrypt;
+const {encrypt, decrypt} = require('../index.js');
 const { e_mail, key } = require('./data.js').data;
 const test_checkHexValue = 'eaaacff9df2e4c2a63083a303d4521f0bd41e375232a2895310179bc030addfb';
 
-const hex_encrypt = encrypt(key, e_mail, 'hex');
+const encrypted = encrypt(key, e_mail, 'hex');
+const hex_encrypted = encrypted.toHex();
 
-if (hex_encrypt !== test_checkHexValue) {
+if (hex_encrypted !== test_checkHexValue) {
 	console.log('invalid encrypted value!');
 	console.table({
 		'should be: ': test_checkHexValue,
-		'is': hex_encrypt
+		'is': hex_encrypted
 	});
 }
 
-console.log('encrytion correct: ', hex_encrypt);
+const decrypted = decrypt(key, encrypted);
+
+if (decrypted.data !== e_mail) {
+	console.log('invalid decrypted value!');
+	console.table({
+		'should be: ': e_mail,
+		'is': decrypted.data
+	});
+}
+
+console.table({ test_content: e_mail, decrypt: decrypted.data });
