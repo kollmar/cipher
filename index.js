3e63851ac24c686dacfe14dc95db3ac5f9209dea
Andre Kollmar <andre.kollmar@raven51.de>
2022-07-18 14:33:21 +0200

    Add functions

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const { Buffer } = require('buffer');
 const forge = require('node-forge');
 const encoder = new TextEncoder();
 
-const encrypt = (apisecret, message, type) => {
-	const keybytes = encoder.encode(apisecret);
-	const buf_keybytes = Buffer.from(keybytes, 'utf8');
-	const key_byteBuffer = forge.util.createBuffer(buf_keybytes.slice(0,16), 'utf8');
-	
-	const iv = Buffer.alloc(16);
-	const iv_byteBuffer = forge.util.createBuffer(iv);
+const encrypt = (apisecret, message) => {
+	const key_byteBuffer = get_secret_byte_buffer(apisecret);
+	const iv_byteBuffer = create_initial_vector(16);
 
 	const message_buffer = forge.util.createBuffer(message, 'utf8');
 	
@@ -18,14 +14,38 @@ const encrypt = (apisecret, message, type) => {
 	cipher.update(message_buffer);
 	cipher.finish();
 
-	const encrypted = cipher.output;
-	const convertOutput = {
-		hex: encrypted.toHex(),
-		default: encrypted
-	}
-	return convertOutput[type || 'default'];
+	return cipher.output;
 }
 
-module.exports = {
-	encrypt,
+const decrypt = (apisecret, message) => {
+	const key_byteBuffer = get_secret_byte_buffer(apisecret);
+	const iv_byteBuffer = create_initial_vector(16);
+
+	const decipher = forge.cipher.createDecipher('AES-CBC', key_byteBuffer);
+
+	decipher.start({ iv: iv_byteBuffer });
+	decipher.update(message);
+	decipher.finish();
+
+	return decipher.output;
 }
+
+const get_secret_byte_buffer = (secret, options = { slice: [0, 16] }) => {
+	const { slice } = options;
+	const secret_content = encoder.encode(secret);
+	let buf_keybytes = Buffer.from(secret_content, 'utf8');
+
+	if (slice !== undefined && slice.length !== 0) {
+		const [begin, end] = slice;
+		buf_keybytes = buf_keybytes.slice(begin, end);
+	}
+	
+	return forge.util.createBuffer(buf_keybytes, 'utf8');
+}
+
+const create_initial_vector = (length) => {
+	const iv = Buffer.alloc(length);
+	return forge.util.createBuffer(iv);
+}
+
+module.exports = { encrypt, decrypt };
