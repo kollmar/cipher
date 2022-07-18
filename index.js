a6c3dc846b060ffd154330d105a5a4a563b2cafe
Andre Kollmar <andre.kollmar@raven51.de>
2022-07-18 15:24:53 +0200

    Refactor to class

--- index.js
+++ index.js
@@ -2,50 +2,69 @@ const { Buffer } = require('buffer');
 const forge = require('node-forge');
 const encoder = new TextEncoder();
 
-const encrypt = (apisecret, message) => {
-	const key_byteBuffer = get_secret_byte_buffer(apisecret);
-	const iv_byteBuffer = create_initial_vector(16);
+class Cipher {
+	constructor(secret, message) {
+		this.secret = secret;
+		this.message = message;
+	}
 
-	const message_buffer = forge.util.createBuffer(message, 'utf8');
+	encrypt(apisecret = this.secret, message = this.message) {
+		const key_byteBuffer = this.#get_secret_byte_buffer(apisecret);
+		const iv_byteBuffer = this.#create_initial_vector(16);
 	
-	const cipher = forge.cipher.createCipher('AES-CBC', key_byteBuffer);
-
-	cipher.start({ iv:  iv_byteBuffer});
-	cipher.update(message_buffer);
-	cipher.finish();
-
-	return cipher.output;
-}
-
-const decrypt = (apisecret, message) => {
-	const key_byteBuffer = get_secret_byte_buffer(apisecret);
-	const iv_byteBuffer = create_initial_vector(16);
-
-	const decipher = forge.cipher.createDecipher('AES-CBC', key_byteBuffer);
-
-	decipher.start({ iv: iv_byteBuffer });
-	decipher.update(message);
-	decipher.finish();
-
-	return decipher.output;
-}
-
-const get_secret_byte_buffer = (secret, options = { slice: [0, 16] }) => {
-	const { slice } = options;
-	const secret_content = encoder.encode(secret);
-	let buf_keybytes = Buffer.from(secret_content, 'utf8');
-
-	if (slice !== undefined && slice.length !== 0) {
-		const [begin, end] = slice;
-		buf_keybytes = buf_keybytes.slice(begin, end);
+		const message_buffer = forge.util.createBuffer(message, 'utf8');
+		
+		const cipher = forge.cipher.createCipher('AES-CBC', key_byteBuffer);
+	
+		cipher.start({ iv:  iv_byteBuffer});
+		cipher.update(message_buffer);
+		cipher.finish();
+	
+		return cipher.output;
 	}
 	
-	return forge.util.createBuffer(buf_keybytes, 'utf8');
+	decrypt(apisecret = this.secret, encrypted_message) {
+		encrypted_message = encrypted_message || this.encrypt(apisecret, this.message);
+		const {
+			secret: key_byteBuffer,
+			iv: iv_byteBuffer
+		} = this.#get_byte_buffer_secret_and_iv(apisecret, 16);
+		
+		const decipher = forge.cipher.createDecipher('AES-CBC', key_byteBuffer);
+	
+		decipher.start({ iv: iv_byteBuffer });
+		decipher.update(encrypted_message);
+		decipher.finish();
+	
+		return decipher.output;
+	}
+	
+	#get_secret_byte_buffer(secret, options = { slice: [0, 16] }) {		
+		const { slice } = options;
+		const secret_content = encoder.encode(secret);
+		let buf_keybytes = Buffer.from(secret_content, 'utf8');
+	
+		if (slice !== undefined && slice.length !== 0) {
+			const [begin, end] = slice;
+			buf_keybytes = buf_keybytes.slice(begin, end);
+		}
+		
+		return forge.util.createBuffer(buf_keybytes, 'utf8');
+	}
+	
+	#create_initial_vector (length) {
+		const iv = Buffer.alloc(length);
+		return forge.util.createBuffer(iv);
+	}
+
+	#get_byte_buffer_secret_and_iv(secret, length) {
+		return {
+			secret: this.#get_secret_byte_buffer(secret),
+			iv: this.#create_initial_vector(length),
+		}
+	}
 }
 
-const create_initial_vector = (length) => {
-	const iv = Buffer.alloc(length);
-	return forge.util.createBuffer(iv);
+module.exports = {
+	Cipher
 }
-
-module.exports = { encrypt, decrypt };
