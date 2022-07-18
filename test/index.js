a3129818d342bcb5c83db3e1e8802a086daf40fe
Andre Kollmar <andre.kollmar@raven51.de>
2022-07-18 15:25:10 +0200

    Modify to class

--- test/index.js
+++ test/index.js
@@ -1,10 +1,13 @@
-const {encrypt, decrypt} = require('../index.js');
+const { Cipher } = require('../index.js');
 const { e_mail, key } = require('./data.js').data;
 const test_checkHexValue = 'eaaacff9df2e4c2a63083a303d4521f0bd41e375232a2895310179bc030addfb';
 
-const encrypted = encrypt(key, e_mail, 'hex');
+const testing = new Cipher(key, e_mail);
+
+const encrypted = testing.encrypt();
 const hex_encrypted = encrypted.toHex();
 
+console.log(hex_encrypted);
 if (hex_encrypted !== test_checkHexValue) {
 	console.log('invalid encrypted value!');
 	console.table({
@@ -13,7 +16,7 @@ if (hex_encrypted !== test_checkHexValue) {
 	});
 }
 
-const decrypted = decrypt(key, encrypted);
+const decrypted = testing.decrypt();
 
 if (decrypted.data !== e_mail) {
 	console.log('invalid decrypted value!');
