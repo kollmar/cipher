c73eda7b8ab2da7354fb913497d23eac1ee1df95
Andre Kollmar <andre.kollmar@raven51.de>
2022-07-18 10:48:58 +0200

    outsource data

--- test/index.js
+++ test/index.js
@@ -1,8 +1,5 @@
-
 const encrypt = require('../index.js').encrypt;
-const data = require('./test.json');
-const e_mail = data.mail;
-const test_key = 'your api secret key';
+const { e_mail, key } = require('./data.js').data;
 const test_checkHexValue = 'eaaacff9df2e4c2a63083a303d4521f0bd41e375232a2895310179bc030addfb';
 
 const hex_encrypt = encrypt(key, e_mail, 'hex');
